f19712adb0a99e8a34be8d0e6614d50a460eea06
Aryasatya-717 <aryasatyahm717@gmail.com>
2025-08-19 10:31:10 +0700

    test

--- main.js
+++ main.js
@@ -139,3 +139,10 @@ var swiper = new Swiper(".mySkillsSwiper", {
     disableOnInteraction: false,
   },
 });
+
+const menuBtn = document.getElementById("menu-btn");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+  });
